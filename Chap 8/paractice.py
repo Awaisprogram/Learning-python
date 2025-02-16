a45b3997857a0ea568c51c2f92444f6d89375b22
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-02-17 04:46:50 +0500

    Update paractice.py

--- Chap 8/paractice.py
+++ Chap 8/paractice.py
@@ -1,41 +1,41 @@
 #1
 
-#Table using for loop
+Table using for loop
 
-# n = int(input("Enter a number: "))
-# for i in range(1,11):
-#   print(f"{n} x {i} = {n * i}")
+n = int(input("Enter a number: "))
+for i in range(1,11):
+  print(f"{n} x {i} = {n * i}")
 
 #2
 
 #Table using While loop
 
-# n = int(input("Enter a number: "))
-# i = 0
-# while i < 10:
-#   i += 1
-#   print(f"{n} x {i} = {n * i}")
+n = int(input("Enter a number: "))
+i = 0
+while i < 10:
+  i += 1
+  print(f"{n} x {i} = {n * i}")
 
 #3
 
 #Greetings
 
-# l = ["Awais", "Waleed", "Ali", "Zahir"]
-# for name in l:
-#   if(name.startswith("A")):
-#     print(f"Greetings {name}")
+l = ["Awais", "Waleed", "Ali", "Zahir"]
+for name in l:
+  if(name.startswith("A")):
+    print(f"Greetings {name}")
 
 # 4 
 
 # sum
 
-# n = int(input("Enter a number: "))
-# i = 1
-# sum = 0
-# while(i<=n):
-#   sum += i
-#   i += 1
-# print(sum)
+n = int(input("Enter a number: "))
+i = 1
+sum = 0
+while(i<=n):
+  sum += i
+  i += 1
+print(sum)
 
 # 4 
 
@@ -43,11 +43,11 @@
 #  ***
 # *****
 
-# n = int(input("Enter a number: "))
-# for i in range(1 , n+1):
-#   print(" " * (n-i) ,end="")
-#   print("*" * (2*i-1),end='')
-#   print('')
+n = int(input("Enter a number: "))
+for i in range(1 , n+1):
+  print(" " * (n-i) ,end="")
+  print("*" * (2*i-1),end='')
+  print('')
 
 # 5 
 
