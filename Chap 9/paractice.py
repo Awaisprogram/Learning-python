0d2a8fd44f4b7375a59d5da6abc34ab7b393902f
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-02-18 00:10:17 +0500

    Add files via upload

---
+++ Chap 9/paractice.py
@@ -0,0 +1,27 @@
+# 1
+
+# def fun(name):
+#   print(f"Have a nice day {name}")
+# fun("Awais")  
+
+#2
+
+# def fun2():
+#   a = int( input("Enter a number:"))
+#   b = int( input("Enter a number:"))
+#   result =  a+b
+
+#   print(result)  
+# fun2()
+
+#3
+
+def fun3(n):
+    if n == 1:
+        return 1 
+    return fun3(n - 1) + n
+print(fun3(4))
+
+
+       
+ 
