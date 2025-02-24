aa12d70cbac992a83897f4e89c00e9da6e15ae45
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-02-24 22:47:39 +0500

    Update problems.py

--- Chap 10/problems.py
+++ Chap 10/problems.py
@@ -3,85 +3,85 @@ from random import randint
 # 1
 # Method 1
 
-# class programmer:
-#   company = "Microsoft"
-#   name = "Awais"
-#   language = "Python"
-#   salary = "120000"
+class programmer:
+  company = "Microsoft"
+  name = "Awais"
+  language = "Python"
+  salary = "120000"
 
-# Awais = programmer()
-# print(Awais.name , Awais.language, Awais.salary)
+Awais = programmer()
+print(Awais.name , Awais.language, Awais.salary)
 
 # Method 2
 
-# class programmer:
-#   company = "Microsoft"
-#   def __init__(self, name, salary, pin):
-#     self.name = name
-#     self.salary = salary
-#     self.pin = pin    
+class programmer:
+  company = "Microsoft"
+  def __init__(self, name, salary, pin):
+    self.name = name
+    self.salary = salary
+    self.pin = pin    
     
-# Awais = programmer("Awais", 120000, 50001 )
-# print(Awais.company, Awais.name , Awais.salary, Awais.pin)
+Awais = programmer("Awais", 120000, 50001 )
+print(Awais.company, Awais.name , Awais.salary, Awais.pin)
 
 # 2
 
-# class Calculator:
-#   def __init__(self, n):
-#     self.n = n
+class Calculator:
+  def __init__(self, n):
+    self.n = n
   
-#   def square(self):
-#     print(f"The square of {self.n} = {self.n*self.n}")
+  def square(self):
+    print(f"The square of {self.n} = {self.n*self.n}")
 
-#   def cube(self):
-#     print(f"The cube of {self.n} = {self.n*self.n*self.n}") 
+  def cube(self):
+    print(f"The cube of {self.n} = {self.n*self.n*self.n}") 
 
-#   def squareroot(self):
-#     print(f"The squareroot of {self.n} = {self.n**1/2}")
+  def squareroot(self):
+    print(f"The squareroot of {self.n} = {self.n**1/2}")
 
 
-# a = Calculator(int(input("Enter a number: ")))
-# a.square()
-# a.cube()
-# a.squareroot()
+a = Calculator(int(input("Enter a number: ")))
+a.square()
+a.cube()
+a.squareroot()
 
 # 3
 
-# class Demo():
-#   a = 4
-# o = Demo()
-# print(o.a)  # Print 4
-# o.a = 0
-# print(o.a)  # Print 0
-# print(Demo.a) # Print 4
+class Demo():
+  a = 4
+o = Demo()
+print(o.a)  # Print 4
+o.a = 0
+print(o.a)  # Print 0
+print(Demo.a) # Print 4
 
 # It Does not change class attribute just priorotize instance attribute
 
 # 4
 
-# class Calculator:
+class Calculator:
 
-#   @staticmethod
-#   def greet():
-#     print("Hello user")
-#   def __init__(self, n):
-#     self.n = n
+  @staticmethod
+  def greet():
+    print("Hello user")
+  def __init__(self, n):
+    self.n = n
   
-#   def square(self):
-#     print(f"The square of {self.n} = {self.n*self.n}")
+  def square(self):
+    print(f"The square of {self.n} = {self.n*self.n}")
 
-#   def cube(self):
-#     print(f"The cube of {self.n} = {self.n*self.n*self.n}") 
+  def cube(self):
+    print(f"The cube of {self.n} = {self.n*self.n*self.n}") 
 
-#   def squareroot(self):
-#     print(f"The squareroot of {self.n} = {self.n**1/2}")
+  def squareroot(self):
+    print(f"The squareroot of {self.n} = {self.n**1/2}")
 
 
-# a = Calculator(int(input("Enter a number: ")))
-# a.greet()
-# a.square()
-# a.cube()
-# a.squareroot()
+a = Calculator(int(input("Enter a number: ")))
+a.greet()
+a.square()
+a.cube()
+a.squareroot()
 
 # 5
 
